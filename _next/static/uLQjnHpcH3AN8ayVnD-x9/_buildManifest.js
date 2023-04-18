e99603606a60fd58efd22eed0a369da41530e665
kou72 <kou72@users.noreply.github.com>
2023-04-18 17:40:32 +0000

    deploy: f5916da9f2cfa2d2d49b3e81a788dc4a226f1915

--- _next/static/uLQjnHpcH3AN8ayVnD-x9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/dfbb8cd53df93af3.css","static/chunks/pages/index-00264f122172142b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
