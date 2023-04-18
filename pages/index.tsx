4183e21a849a2f797a83da30137b32777d1c8311
kou72 <genpokopokopoko@gmail.com>
2023-04-19 03:06:30 +0900

    StaticImageコンポーネントを作成し、画像のパスを動的に生成するように変更
    component/StaticImage.tsx
    - ReactとisProdをインポート
    - isProdがtrueの場合は、prefixPathを空文字列に、falseの場合は"/sub-directory"に設定
    - withPrefix関数を定義し、引数のpathにprefixPathを付与した文字列を返す
    - StaticImagePropsという型を定義し、src, alt, className, width, heightを受け取る
    - StaticImageコンポーネントを定義し、imgタグを返す。src属性にwithPrefix(src)を、alt属性にaltを設定する
    
    pages/index.tsx
    - ImageとStaticImageをインポート
    - 画像のパスを動的に生成するため、ImageコンポーネントをStaticImageコンポーネントに置き換える
    - InterとuseRouterをインポートしていたが、使用していないため削除
    - 画像のパスを動的に生成するため、imgタグをStaticImageコンポーネントに置き換える

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
+import StaticImage from '../component/StaticImage'
 import { Inter } from 'next/font/google'
-import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const router = useRouter()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -21,26 +20,24 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <img
-              src={router.pathname + '/vercel.svg'}
+            <StaticImage
+              src="/portfolio/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
               height={24}
-              // priority
             />
           </a>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <img
+        <StaticImage
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src={router.pathname + '/next.svg'}
+          src="/portfolio/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
-          // priority
         />
       </div>
 
