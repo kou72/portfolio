a4e16f19393880ef3cd2f2213dbcabc2e2e689e5
kou72 <genpokopokopoko@gmail.com>
2023-05-14 04:44:52 +0900

    ボタンのテキストを変更し、GitHub、Zenn、Twitterのロゴを追加
    pages/index.tsx
    - 業務経歴のボタンのテキストを「業務・活動の履歴」に変更
    - 作ったサービスのボタンのテキストを「作ったサービス・教材」に変更
    - GitHub、Zenn、Twitterのロゴを追加
    
    public/github-logo.svg
    - GitHubのロゴを追加
    
    public/twitter-logo.svg
    - Twitterのロゴを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,20 +9,38 @@ export default function Home() {
           <div className="flex justify-center text-6xl font-mono font-extrabold text-gray-700 p-12">
             Kou S
           </div>
-          <button className="border border-gray-300 rounded-3xl px-24 py-4 text-xl hover:bg-gray-100 transition-colors duration-500">
-            業務経歴
+          <button className="border border-gray-300 rounded-3xl py-4 text-xl hover:bg-gray-100 transition-colors duration-500">
+            業務・活動の履歴
           </button>
-          <button className="border border-gray-300 rounded-3xl px-24 py-4 text-xl hover:bg-gray-100 transition-colors duration-500">
-            作ったサービス
+          <button className="border border-gray-300 rounded-3xl py-4 text-xl hover:bg-gray-100 transition-colors duration-500">
+            作ったサービス・教材
           </button>
-          <button className="border border-gray-300 rounded-3xl hover:bg-gray-100 transition-colors duration-500">
-            <StaticImage
-              src={'/zenn-logo.svg'}
-              alt={'zenn'}
-              width={16}
-              height={16}
-            ></StaticImage>
-          </button>
+          <div className="flex justify-evenly">
+            <button className="flex items-center justify-center border border-gray-300 rounded-3xl w-16 h-16 hover:bg-gray-100 transition-colors duration-500">
+              <StaticImage
+                src={'/github-logo.svg'}
+                alt={'github'}
+                width={32}
+                height={32}
+              ></StaticImage>
+            </button>
+            <button className="flex items-center justify-center border border-gray-300 rounded-3xl p-4 hover:bg-gray-100 transition-colors duration-500">
+              <StaticImage
+                src={'/zenn-logo.svg'}
+                alt={'zenn'}
+                width={32}
+                height={32}
+              ></StaticImage>
+            </button>
+            <button className="flex items-center justify-center border border-gray-300 rounded-3xl p-4 hover:bg-gray-100 transition-colors duration-500">
+              <StaticImage
+                src={'/twitter-logo.svg'}
+                alt={'twitter'}
+                width={32}
+                height={32}
+              ></StaticImage>
+            </button>
+          </div>
         </div>
       </div>
     </>
