5ce420a70e677e25c593730264a8492b017c7e69
kou72 <genpokopokopoko@gmail.com>
2023-05-14 04:26:12 +0900

    StaticImage.tsxファイルで、prefixPathの値を変更するための条件式を修正
    StaticImage.tsx
    - isProd変数がtrueの場合、prefixPathに"/portfolio"を代入するように変更
    - isProd変数がfalseの場合、prefixPathに空文字列を代入するように変更

--- component/StaticImage.tsx
+++ component/StaticImage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 const isProd = process.env.NODE_ENV === 'production'
-const prefixPath = !isProd ? '' : ''
+const prefixPath = isProd ? '/portfolio' : ''
 
 function withPrefix(path: string) {
   return `${prefixPath}${path}`
