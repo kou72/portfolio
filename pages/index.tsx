423ee65e96852b0e9fa808eb9d2001686ac1ba96
kou72 <genpokopokopoko@gmail.com>
2023-05-14 03:24:33 +0900

    index.tsxファイルで、ページのレイアウトを変更し、自分の名前を表示するように修正
    index.tsx
    - mainタグにmin-h-screenクラスを追加して、ページ全体を表示するように変更
    - divタグを追加して、ヘッダーを作成
    - divタグを追加して、自分の名前を表示するように変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="">Hello World</h1>
+    <main className="min-h-screen">
+      <div className="w-full h-12 border-b border-gray-300"></div>
+      <div className="flex justify-center text-6xl font-mono font-extrabold text-gray-700 p-16">
+        Kou S
+      </div>
     </main>
   )
 }
