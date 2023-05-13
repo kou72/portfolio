e46d6116c12a43e42611350efe7f5f5605901ce5
kou72 <genpokopokopoko@gmail.com>
2023-05-14 04:02:04 +0900

    トップページのレイアウトを変更し、3つのボタンを追加
    pages/index.tsx
    - mainタグを削除し、代わりにReact.Fragmentタグを追加
    - Kou Sという文字列を表示するdivタグを、ボタンを含むdivタグに変更
    - 業務経歴、作ったサービス、Zennの3つのボタンを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <div className="w-full h-12 border-b border-gray-300"></div>
-      <div className="flex justify-center text-6xl font-mono font-extrabold text-gray-700 p-16">
-        Kou S
+    <>
+      <div className=" w-full h-12 border-b border-gray-300"></div>
+      <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center gap-4 w-80">
+          <div className="flex justify-center text-6xl font-mono font-extrabold text-gray-700 p-12">
+            Kou S
+          </div>
+          <button className="border border-gray-300 rounded-3xl px-24 py-4 text-xl hover:bg-gray-100 transition-colors duration-500">
+            業務経歴
+          </button>
+          <button className="border border-gray-300 rounded-3xl px-24 py-4 text-xl hover:bg-gray-100 transition-colors duration-500">
+            作ったサービス
+          </button>
+          <button className="border border-gray-300 rounded-3xl px-24 py-4 text-xl hover:bg-gray-100 transition-colors duration-500">
+            Zenn
+          </button>
+        </div>
       </div>
-    </main>
+    </>
   )
 }
