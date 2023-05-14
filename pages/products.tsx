59e5e49bcb0db6a1a2d5667dd8b7ed02af200183
kou72 <genpokopokopoko@gmail.com>
2023-05-14 12:14:10 +0900

    Headerコンポーネントを作成し、products.tsxとworks.tsxで読み込むように修正
    pages/products.tsx
    - "@/components/Header"をインポート
    - Headerコンポーネントをレンダリングするように修正
    
    pages/works.tsx
    - "@/components/Header"をインポート
    - Headerコンポーネントをレンダリングするように修正
    
    components/Header.tsx
    - ヘッダーのコンポーネントを作成
    - ロゴとナビゲーションを表示する

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,10 +1,12 @@
+import { Header } from '@/components/Header'
+
 export default function Home() {
   return (
     <div className="bg-blue-100 min-h-screen">
-      <div className="bg-white w-full h-12 border-b border-gray-300"></div>
+      <Header />
       <div className="flex flex-col justify-center items-center">
-        <div className="flex justify-center text-4xl font-mono font-extrabold text-gray-700 py-12 w-full">
-          作ったサービス・教材
+        <div className="flex justify-center text-4xl font-extrabold text-gray-700 py-12 w-full">
+          Products
         </div>
       </div>
     </div>
--- pages/works.tsx
+++ pages/works.tsx
@@ -1,10 +1,12 @@
+import { Header } from '@/components/Header'
+
 export default function Home() {
   return (
     <div className="bg-blue-100 min-h-screen">
-      <div className="bg-white w-full h-12 border-b border-gray-300"></div>
+      <Header />
       <div className="flex flex-col justify-center items-center">
-        <div className="flex justify-center text-4xl font-mono font-extrabold text-gray-700 py-12 w-full">
-          業務・活動の履歴
+        <div className="flex justify-center text-4xl font-extrabold text-gray-700 py-12 w-full">
+          Works
         </div>
       </div>
     </div>
