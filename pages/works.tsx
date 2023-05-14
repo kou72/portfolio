ae1bd443898419f6836b5649d3f9988c7e25cdef
kou72 <genpokopokopoko@gmail.com>
2023-05-14 16:36:34 +0900

    works.tsxファイルを修正し、マークダウンの表示を修正、業務経歴を追加
    - マークダウンの表示を修正
      - div要素に背景色を設定し、マークダウンの表示を囲むように修正
    - 業務経歴を追加
      - ネットワーク構築業務について、期間と概要を追加
      - 使用した機器やソフトウェアについて、バッククォートで囲んで追加

--- pages/works.tsx
+++ pages/works.tsx
@@ -9,8 +9,10 @@ export default function Works() {
         <div className="flex justify-center text-4xl font-extrabold text-gray-700 py-12 w-full">
           Works
         </div>
-        <div className="markdown-body">
-          <ReactMarkdown>{text}</ReactMarkdown>
+        <div className="flex flex-col gap-4 w-80">
+          <div className="bg-white w-full rounded-3xl p-4">
+            <ReactMarkdown className="markdown-body">{text}</ReactMarkdown>
+          </div>
         </div>
       </div>
     </div>
@@ -18,14 +20,11 @@ export default function Works() {
 }
 
 const text = `
-# h1
-## h2
-### h3
+## ネットワーク構築業務
+2018/4 ~ 2023/3
 
-- list1
-- list2
-- list3
+新卒入社後、ネットワーク構築をメインに従事。ネットワーク設計～構築～実導入まで対応。またフロント対応も実施。
 
-\`test\`
+\`Cisco\` (\`Catalyst\` \`Nexus\` \`ISR\` \`Aironet\`) \`Fortinet\`(\`FortiGate\` \`FortiManager\` \`FortiAnalyzer\`) \`Aruba AP\` \`Palo Alto PA-X\` \`F5 BIG-IP\`
 
 `
