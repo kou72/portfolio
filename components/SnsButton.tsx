bcd3a2fe72efc2646a0e9f475f13868268a1dc95
kou72 <genpokopokopoko@gmail.com>
2023-05-14 11:34:58 +0900

    SnsButtonコンポーネントのスタイル修正、Homeページのスタイル修正
    components/SnsButton.tsx
    - button要素のクラス名から"bg-white"を削除
    
    pages/index.tsx
    - 背景色を指定するために、div要素に"bg-blue-100"クラスを追加
    - Kou Sの名前を表示するためのdiv要素に、"flex-col"クラスを追加
    - Kou Sの名前を表示するためのdiv要素に、"text-6xl"クラスを追加
    - Kou Sの名前を表示するためのdiv要素に、"font-mono"クラスを追加
    - Kou Sの名前を表示するためのdiv要素に、"font-extrabold"クラスを追加
    - Kou Sの名前を表示するためのdiv要素に、"text-gray-700"クラスを追加
    - Kou Sの名前を表示するためのdiv要素に、"py-12"クラスを追加

--- components/SnsButton.tsx
+++ components/SnsButton.tsx
@@ -7,7 +7,7 @@ interface SnsButtonProps {
 
 export const SnsButton = ({ src, alt }: SnsButtonProps) => {
   return (
-    <button className="bg-white flex items-center justify-center rounded-3xl w-16 h-16 hover:bg-gray-200 transition-colors duration-500">
+    <button className="flex items-center justify-center rounded-3xl w-16 h-16 hover:bg-gray-200 transition-colors duration-500">
       <StaticImage src={src} alt={alt} width={32} height={32}></StaticImage>
     </button>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import { SnsButton } from '../components/SnsButton'
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-blue-100 min-h-screen">
       <div className="bg-white w-full h-12 border-b border-gray-300"></div>
-      <div className="flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex justify-center text-6xl font-mono font-extrabold text-gray-700 py-12 w-full">
+          Kou S
+        </div>
         <div className="flex flex-col justify-center gap-4 w-80">
-          <div className="flex justify-center text-6xl font-mono font-extrabold text-gray-700 p-12">
-            Kou S
-          </div>
           <SimpleButton>業務・活動の履歴</SimpleButton>
           <SimpleButton>作ったサービス・教材</SimpleButton>
           <div className="flex justify-evenly">
