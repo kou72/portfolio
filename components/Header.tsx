8d6243cd981ed843b3a094c395e895f0db8bdf8e
kou72 <genpokopokopoko@gmail.com>
2023-05-14 11:44:45 +0900

    Headerコンポーネントを新規作成し、indexページで利用するように修正
    components/Header.tsx
    - Headerコンポーネントを定義
    - div要素を返す
    - classNameに"bg-white w-full h-12 border-b border-gray-300"を指定
    
    pages/index.tsx
    - Headerコンポーネントをインポート
    - Headerコンポーネントを呼び出すように修正
    - SnsButtonコンポーネントの閉じタグを自己終了タグに修正

---
+++ components/Header.tsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+  return <div className="bg-white w-full h-12 border-b border-gray-300"></div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import { Header } from '@/components/Header'
 import { SimpleButton } from '../components/SimpleButton'
 import { SnsButton } from '../components/SnsButton'
 
 export default function Home() {
   return (
     <div className="bg-blue-100 min-h-screen">
-      <div className="bg-white w-full h-12 border-b border-gray-300"></div>
+      <Header />
       <div className="flex flex-col justify-center items-center">
         <div className="flex justify-center text-6xl font-mono font-extrabold text-gray-700 py-12 w-full">
           Kou S
@@ -13,9 +14,9 @@ export default function Home() {
           <SimpleButton text={'業務・活動の履歴'} href={'/works'} />
           <SimpleButton text={'作ったサービス・教材'} href={'/products'} />
           <div className="flex justify-evenly">
-            <SnsButton src={'/github-logo.svg'} alt={'github'}></SnsButton>
-            <SnsButton src={'/zenn-logo.svg'} alt={'zenn'}></SnsButton>
-            <SnsButton src={'/twitter-logo.svg'} alt={'twitter'}></SnsButton>
+            <SnsButton src={'/github-logo.svg'} alt={'github'} />
+            <SnsButton src={'/zenn-logo.svg'} alt={'zenn'} />
+            <SnsButton src={'/twitter-logo.svg'} alt={'twitter'} />
           </div>
         </div>
       </div>
