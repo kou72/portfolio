8893b54961fa7c6b10aa24ec1e7c4a126e76d4b3
kou72 <genpokopokopoko@gmail.com>
2023-05-17 01:03:31 +0900

    SimpleButtonコンポーネントのコードを読み込み、SyntaxHighlighterで表示するように修正
    pages/index.tsx
    - fsモジュールをインポート
    - SyntaxHighlighter、oneDark、GetStaticPropsをインポート
    - Homeコンポーネントにpropsを追加し、simpleButtonCodeを受け取るように修正
    - getStaticProps関数を追加し、SimpleButtonコンポーネントのコードを読み込んでsimpleButtonCodeとしてpropsに渡すように修正
    - SimpleButtonコンポーネントのコードをSyntaxHighlighterで表示するように修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
+import fs from 'fs'
 import { Header } from '@/components/Header'
 import { SimpleButton } from '../components/SimpleButton'
 import { SnsButton } from '../components/SnsButton'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { GetStaticProps } from 'next'
 
-export default function Home() {
+export default function Home(props: any) {
+  console.log(props.simpleButtonCode)
+
   return (
     <div className="bg-blue-100 min-h-screen">
       <Header />
@@ -13,6 +19,9 @@ export default function Home() {
         <div className="flex flex-col justify-center gap-4 w-80">
           <SimpleButton text={'業務・活動の履歴'} href={'/works'} />
           <SimpleButton text={'作ったサービス・教材'} href={'/products'} />
+          <SyntaxHighlighter language="javascript" style={oneDark}>
+            {props.simpleButtonCode}
+          </SyntaxHighlighter>
           <div className="flex justify-evenly">
             <SnsButton src={'/github-logo.svg'} alt={'github'} />
             <SnsButton src={'/zenn-logo.svg'} alt={'zenn'} />
@@ -22,4 +31,16 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = () => {
+  const simpleButtonCode = fs.readFileSync(
+    'components/SimpleButton.tsx',
+    'utf8'
+  )
+  return {
+    props: {
+      simpleButtonCode: simpleButtonCode,
+    },
+  }
 }
