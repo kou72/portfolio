fde146bbcb8d38e5ed30f5b06d917f5d48d138e0
kou72 <genpokopokopoko@gmail.com>
2023-05-17 03:08:40 +0900

    WorksページにCodeBlockコンポーネントを追加し、PropsTypeを定義
    works.tsx
    - CodeBlockコンポーネントをインポート
    - PropsTypeを定義し、markdowns、workCardPath、workCardCodeを受け取るように変更
    - Works関数の引数をPropsTypeに変更
    - WorkCardコンポーネントのdiv要素にmb-4を追加
    - WorkCardコンポーネントのmap関数内にCodeBlockコンポーネントを追加
    - getStaticProps関数内で、workCardPathとworkCardCodeを定義し、PropsTypeに追加

--- pages/works.tsx
+++ pages/works.tsx
@@ -2,18 +2,33 @@ import fs from 'fs'
 import { Header } from '@/components/Header'
 import { GetStaticProps } from 'next'
 import { WorkCard } from '@/components/WorkCard'
+import { CodeBlock } from '@/components/CodeBloc'
 
-export default function Works(props: any) {
+interface PropsType {
+  markdowns: string[]
+  workCardPath: string
+  workCardCode: string
+}
+
+export default function Works(props: PropsType) {
   return (
     <div className="bg-blue-100 min-h-screen">
       <Header />
-      <div className="flex flex-col justify-center items-center mb-4">
+      <div className="flex flex-col justify-center items-center">
         <div className="flex justify-center text-4xl font-extrabold text-gray-700 py-12 w-full">
           Works
         </div>
-        <div className="flex flex-col gap-4 w-80">
-          {props.markdowns.map((markdown: any, index: number) => (
-            <WorkCard markdown={markdown} key={index} />
+        <div className="flex flex-col gap-4 w-80 mb-4">
+          {props.markdowns.map((markdown, index) => (
+            <>
+              <WorkCard markdown={markdown} key={index} />
+              {index === 0 && (
+                <CodeBlock
+                  path={props.workCardPath}
+                  code={props.workCardCode}
+                />
+              )}
+            </>
           ))}
         </div>
       </div>
@@ -41,9 +56,14 @@ export const getStaticProps: GetStaticProps = () => {
     return markdowns
   }
 
+  const workCardPath = 'components/WorkCard.tsx'
+  const workCardCode = fs.readFileSync(workCardPath, 'utf8')
+
   return {
     props: {
       markdowns: getMarkdownFiles(),
+      workCardPath: workCardPath,
+      workCardCode: workCardCode,
     },
   }
 }
