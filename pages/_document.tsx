03877d6b69216ba8a551bcba174770536f4d56f9
kou72 <genpokopokopoko@gmail.com>
2023-07-31 00:08:12 +0900

    deploy.ymlファイルでcnameを追加、next.config.jsファイルでbasePathを修正、_document.tsxファイルでfaviconを追加
    deploy.yml
    - cnameを'portfolio.skapp.dev'に設定
    
    next.config.js
    - basePathをprocess.env.GITHUB_ACTIONSがtrueの場合は空文字列に設定
    
    _document.tsx
    - Headコンポーネント内にfaviconを追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🏠</text></svg>"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
