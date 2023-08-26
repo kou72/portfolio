b3ecafd16ab06b861a6ca1f25d7f6651af3d92e7
kou72 <genpokopokopoko@gmail.com>
2023-08-26 22:10:44 +0900

    SimpleButton.tsx - ボタンのhover時の背景色のトランジションのdurationを500msから200msに変更
    SnsButton.tsx
    - ボタンのhover時の背景色のトランジションのdurationを500msから200msに変更

--- components/SimpleButton.tsx
+++ components/SimpleButton.tsx
@@ -8,7 +8,7 @@ interface SimpleButtonProps {
 export const SimpleButton = ({ text, href }: SimpleButtonProps) => {
   return (
     <Link href={href}>
-      <button className="bg-white w-full border border-gray-300 rounded-3xl py-4 text-xl hover:bg-gray-200 transition-colors duration-500">
+      <button className="bg-white w-full border border-gray-300 rounded-3xl py-4 text-xl hover:bg-gray-200 transition-colors duration-200">
         {text}
       </button>
     </Link>
--- components/SnsButton.tsx
+++ components/SnsButton.tsx
@@ -9,7 +9,7 @@ interface SnsButtonProps {
 export const SnsButton = ({ src, alt, href }: SnsButtonProps) => {
   return (
     <a href={href} target="_blank">
-      <button className="flex items-center justify-center rounded-3xl w-16 h-16 hover:bg-gray-200 transition-colors duration-500">
+      <button className="flex items-center justify-center rounded-3xl w-16 h-16 hover:bg-gray-200 transition-colors duration-200">
         <StaticImage src={src} alt={alt} width={32} height={32}></StaticImage>
       </button>
     </a>
