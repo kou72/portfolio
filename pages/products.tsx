8cf02c07ab8277b3ad6f7c7f4914273f60bfccec
kou72 <genpokopokopoko@gmail.com>
2023-08-26 19:59:28 +0900

    package.jsonファイルにaxios、jsdom、@types/jsdomの依存関係を追加、products.tsxファイルを新規作成し、外部サイトのHTMLを取得して表示する処理を追加
    package.json
    - axiosバージョン1.4.0を追加
    - jsdomバージョン22.1.0を追加
    - @types/jsdomバージョン21.1.2を追加
    
    pages/products.tsx
    - GetStaticPropsをnextからインポート
    - axiosをインポート
    - jsdomをインポート
    - Productsコンポーネントを追加
    - getStaticProps関数を追加し、外部サイトのHTMLを取得して表示する処理を追加

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,3 +1,6 @@
+import { GetStaticProps } from 'next'
+import axios from 'axios'
+import { JSDOM } from 'jsdom'
 import { Header } from '@/components/Header'
 
 export default function Products() {
@@ -11,4 +14,21 @@ export default function Products() {
       </div>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const url = 'https://meet-time.skapp.dev/'
+  const encodedUri = encodeURI(url)
+  const headers = { 'User-Agent': 'bot' }
+  const response = await axios.get(encodedUri, { headers })
+  const html = response.data
+  const dom = new JSDOM(html)
+  const ogTitleContent = dom.window.document
+    .querySelector('meta[property="og:title"]')
+    ?.getAttribute('content')
+  console.log(ogTitleContent)
+
+  return {
+    props: {},
+  }
 }
