9551f6e6cff66fa4f2692477f6f3994516347602
kou72 <genpokopokopoko@gmail.com>
2023-08-27 02:58:58 +0900

    pages/index.tsxファイルにコンポーネントをcode形式で表示するための修正が追加されている
    pages/index.tsx
    - コンポーネントをcode形式で表示するための修正が追加されている
    - コンポーネントをcode形式で表示するための修正が追加されている
    - コンポーネントをcode形式で表示するための修正が追加されている

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,7 @@ export default function Home(props: PropsType) {
         </div>
         <div className="flex flex-col justify-center gap-4 w-80 m-4">
           <SimpleButton text={'業務・活動の履歴'} href={'/works'} />
+          {/* コンポーネントをcode形式で表示 */}
           <CodeBlock
             path={props.simpleButtonPath}
             code={props.simpleButtonCode}
@@ -36,6 +37,7 @@ export default function Home(props: PropsType) {
             <SnsButton src={'/zenn-logo.svg'} alt={'zenn'} href={zennUrl} />
             <SnsButton src={'/x-logo.svg'} alt={'x'} href={xUrl} />
           </div>
+          {/* コンポーネントをcode形式で表示 */}
           <CodeBlock path={props.snsButtonPath} code={props.snsButtonCode} />
         </div>
       </div>
@@ -43,6 +45,7 @@ export default function Home(props: PropsType) {
   )
 }
 
+// CodeBlockで表示するコンポーネントとコードを取得
 export const getStaticProps: GetStaticProps = () => {
   const simpleButtonPath = 'components/SimpleButton.tsx'
   const simpleButtonCode = fs.readFileSync(simpleButtonPath, 'utf8')
